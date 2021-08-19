a90ede7d4ea35d2b69d8f75e248b55d09551e109
Vadim Makarov <add4che@gmail.com>
2021-08-19 10:38:23 +0300

    fix request to api

--- src/faceAPI.ts
+++ src/faceAPI.ts
@@ -4,14 +4,17 @@ export const submitImage = async (imageURL: string, id: string) => {
     const imageEncoded = imageURL.split(',')[1];
     return fetch(API_URL, {
         method: 'POST',
-        body: JSON.stringify({
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(JSON.stringify({
             image: imageEncoded,
             id: id
-        })
+        }))
     })
         .then(res => res.json())
         .catch((err) => {
-            console.log(err);
+            // console.log(err);
             return null;
         });
 }
