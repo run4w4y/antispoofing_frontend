e3c5a823f98edc13a3e1eb01a256c75cf10fd40b
Vadim Makarov <add4che@gmail.com>
2021-08-19 10:38:33 +0300

    update webcam component

--- src/components/Webcam.tsx
+++ src/components/Webcam.tsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect, useRef } from 'react';
 
 interface WebcamProps {
+    children: JSX.Element,
     faceID: string,
-    submitImage?: (img: string, id: string) => any
+    submitImage?: (img: string, id: string) => any,
+    callback?: () => void
 };
 
 interface FaceArea {
@@ -20,11 +22,15 @@ export const Webcam = (props: WebcamProps) => {
     const [mirrored, setMirrored] = useState<boolean>(false);
     const [currentFrame, setCurrentFrame] = useState<number>(0);
     const [currentFaceArea, setCurrentFaceArea] = useState<FaceArea | null>(null);
+    const [resultSpoofing, setResultSpoofing] = useState<number | null>(null);
+    const [resultFaceScore, setResultFaceScore] = useState<number | null>(null);
+    const [successCount, setSuccessCount] = useState<number>(0);
     const mirroredRef = useRef<HTMLInputElement>(null);
     const selectRef = useRef<HTMLSelectElement>(null);
     const videoRef = useRef<HTMLVideoElement>(null);
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const hiddenCanvasRef = useRef<HTMLCanvasElement>(null);
+    const canvasContainerRef = useRef<HTMLDivElement>(null);
 
     const updateWebcamStream = async () => {
         if (webcamStream) // stop the current stream
@@ -68,10 +74,12 @@ export const Webcam = (props: WebcamProps) => {
         const video = videoRef.current!;
         const canvas = canvasRef.current!;
         const context = canvas.getContext('2d')!;
+        const container = canvasContainerRef.current!;
+        const scaleRatio = container.offsetWidth / video.videoWidth;
         
         const renderFrame = () => { // render the current webcam stream frame in the canvas 
-            canvas.width = video.videoWidth;
-            canvas.height = video.videoHeight;
+            canvas.width = video.videoWidth * scaleRatio;
+            canvas.height = video.videoHeight * scaleRatio;
 
             if (mirroredRef.current?.checked)
                 context.scale(-1, 1);
@@ -83,10 +91,10 @@ export const Webcam = (props: WebcamProps) => {
             // TODO: mirror
             if (currentFaceArea) {
                 context.rect(
-                    currentFaceArea.left, 
-                    currentFaceArea.top, 
-                    currentFaceArea.right, 
-                    currentFaceArea.bottom
+                    currentFaceArea.left * scaleRatio - (mirroredRef.current?.checked ? canvas.width : 0), 
+                    currentFaceArea.top * scaleRatio, 
+                    (currentFaceArea.right - currentFaceArea.left) * scaleRatio, 
+                    (currentFaceArea.bottom - currentFaceArea.top) * scaleRatio
                 );
                 context.lineWidth = 2;
                 context.strokeStyle = currentFaceArea.color;
@@ -101,7 +109,7 @@ export const Webcam = (props: WebcamProps) => {
             const hcontext = hiddenCanvas.getContext('2d')!;
             const 
                 ratio = video.videoHeight / video.videoWidth,
-                w = 800,
+                w = 600,
                 h = ratio * w;
             hiddenCanvas.width = w;
             hiddenCanvas.height = h;
@@ -114,12 +122,17 @@ export const Webcam = (props: WebcamProps) => {
         renderFrame();
         drawFaceArea();
         const submit = async () => {
-            const result = props.submitImage!(encodeFrame(), props.faceID);
+            const result = await props.submitImage!(encodeFrame(), props.faceID);
             console.log(result);
-            setCurrentFaceArea(result.bbox);
+            setCurrentFaceArea({
+                ...result.bbox,
+                color: result.spoofing < 0.5 ? 'green' : 'blue'
+            });
+            setResultFaceScore(result.face_score);
+            setResultSpoofing(result.spoofing);
         };
 
-        if (props.submitImage)
+        if (props.submitImage && currentFrame % 15 === 0)
             submit();
         
         const timeout = setTimeout(
@@ -129,23 +142,53 @@ export const Webcam = (props: WebcamProps) => {
 
     }, [currentFrame]);
 
+    // TODO: optimize for phones
     return (
         <div>
             <video autoPlay={true} className="hidden" ref={videoRef} muted />
-            <canvas ref={canvasRef} />
+            <div className="grid grid-cols-2">
+                <div ref={canvasContainerRef} className="col-span-2 lg:col-span-1">
+                    <canvas ref={canvasRef} />
+                </div>
+                <div className="col-span-2 lg:col-span-1 w-50 p-6 bg-gray-100">
+                    <ul>
+                        <li>
+                            <b>Successful attemps:</b> {successCount}
+                        </li>
+                        <li>
+                            <b>face_score:</b> {resultFaceScore}
+                        </li>
+                        <li>
+                            <b>spoofing:</b> {resultSpoofing}
+                        </li>
+                        <li>
+                            <label>
+                                <b>Select input:</b>
+                                <br />
+                                <select ref={selectRef} onChange={(e) => setActiveDeviceId(e.target.value)}>
+                                    { videoInputs?.map(x => <option value={x.deviceId}> {x.label || `Camera ${x.deviceId}`} </option>)} 
+                                </select>
+                            </label>
+                        </li>
+                        <li>
+                            <label className="flex items-center">
+                                <b className="pr-2 flex-col">Mirror image:</b>
+                                <input className="flex-col" type='checkbox' ref={mirroredRef} checked={mirrored} onChange={
+                                    (e) => {
+                                        setMirrored(e.target.checked);
+                                        console.log(mirrored);
+                                    }
+                                } />
+                            </label> 
+                        </li>
+                        <li>
+                            {props.children}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            
             <canvas ref={hiddenCanvasRef} className="hidden" />
-            <select ref={selectRef} onChange={(e) => setActiveDeviceId(e.target.value)}>
-                { videoInputs?.map(x => <option value={x.deviceId}> {x.label || `Camera ${x.deviceId}`} </option>)} 
-            </select>
-            <label>
-                Mirror image:
-                <input type='checkbox' ref={mirroredRef} checked={mirrored} onChange={
-                    (e) => {
-                        setMirrored(e.target.checked);
-                        console.log(mirrored);
-                    }
-                } />
-            </label> 
         </div>
     );
 }
