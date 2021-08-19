24411cbddb2130a1ade4cc10e9a71b068c21abb6
Vadim Makarov <add4che@gmail.com>
2021-08-19 10:38:03 +0300

    update nix shell

--- shell.nix
+++ shell.nix
@@ -1,9 +1,25 @@
-{ pkgs ? import <nixpkgs> {}, ... }:
+{ pkgs ? import <nixos-unstable> {}, ... }:
 
+let 
+    python = 
+        let packageOverrides = self: super: {
+            opencv4 = super.opencv4.override {
+                enableGtk2 = true;
+                gtk2 = pkgs.gtk2;
+            };
+        };
+        in
+            pkgs.python39.override { inherit packageOverrides; self = python; };
+in
 pkgs.mkShell {
     name = "antispoofing_frontend_dev";
     buildInputs = with pkgs; [
         nodejs
         yarn
+        (python.withPackages (p: with p; [
+            opencv4
+            tqdm
+            requests
+        ]))
     ];
 }
