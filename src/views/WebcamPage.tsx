14184a013d6255aff4dde47744154ae46b24b4f8
Vadim Makarov <add4che@gmail.com>
2021-08-19 11:01:43 +0300

    update WebcamPage view

--- src/views/WebcamPage.tsx
+++ src/views/WebcamPage.tsx
@@ -1,19 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
-import { Webcam } from '../components'
+import { Webcam, Timer } from '../components'
 import { submitImage } from '../faceAPI'
 
 export function WebcamPage() {
     const location = useLocation();
     const seed = new URLSearchParams(location.search).get('seed');
     const { faceID } = useParams<{ faceID: string }>();
+    const [ expired, setExpired ] = useState(false);
+    const [ passed, setPassed ] = useState(false);
 
     if (!faceID || !seed)
-        return <div> 404 </div>;
+        return <div></div>;
     
+    if (expired)
+        return <div>Time limit was reached.</div>;
+
+    if (passed)
+        return <div>You passed, here's you key: </div>;
+
     return (
         <div> 
-            <Webcam faceID={faceID} submitImage={submitImage} />
+            <Webcam callback={() => setPassed(true)} faceID={faceID} submitImage={submitImage}>
+                <div>
+                    <b>Time left:</b> 
+                    <br />
+                    <Timer seconds={600} callback={() => setExpired(true)} />
+                </div>
+            </Webcam>
         </div>
     );
 }
